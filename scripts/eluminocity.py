6effea81dee4a53438465d1abeb0e08b89d2773c
Joao Curcio <joao8177@gmail.com>
2016-06-24 15:21:06 -0500

    Enhance portability

--- scripts/eluminocity.py
+++ scripts/eluminocity.py
@@ -1,21 +1,36 @@
 import urllib2
 import time, threading
 
+STATE_FILEPATH = "state.txt" 
+
 # downloads a url and returns a list with it's contents
 def getStateFile(url):
     response = urllib2.urlopen(url)
     html = response
     return html
 
-def parse(timestamp, state_file):
+def parse(timestamp, url):
     entry = timestamp
+    header = "timestamp"
+    state_file = getStateFile(url)
     for line in state_file:
         line_arr = line.rstrip().split(':')
         state = line_arr[0]
         value = line_arr[1]
+        header += ','+state
         entry += ',' + value
     entry += '\n'
-    return entry
+    header += '\n'
+    return entry, header
+
+def initFile(filename, header):
+    try:
+        fp = open(filename)
+    except IOError:
+        # If not exists, create the file
+        fp = open(filename, 'w+')
+        fp.write(header)
+        fp.close()
 
 def saveLine(filename, csv):
     f = open(filename, 'a')
@@ -24,10 +39,11 @@ def saveLine(filename, csv):
 
 def main():
     timestamp = time.ctime()
-    state_file = getStateFile("http://192.168.123.123/rest/full_state")
-    csv = parse(timestamp, state_file)
-    saveLine('state.txt', csv)
-    threading.Timer(10, main).start() # periodically calls main every 60 seconds
+    csv, header = parse(timestamp, "http://192.168.123.123/rest/full_state")
+    saveLine(STATE_FILEPATH, csv)
+    threading.Timer(1, main).start() # periodically calls main every 60 seconds
     
 if __name__ == '__main__':
+    csv, header = parse('',"http://192.168.123.123/rest/full_state")
+    initFile(STATE_FILEPATH, header)
     main()
